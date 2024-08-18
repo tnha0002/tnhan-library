fe3af137054843bf1a253666a4b4d32324e2ad84
Tri Thong Nhan <tnha0002@student.monash.edu>
2024-08-18 13:36:39 +1000

    Week 3 Applied: Completed

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -101,4 +101,19 @@ const submitForm = () => {
 }
 </script>
 
-<style></style>
+<style>
+.card {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.card-header {
+  background-color: #275fda;
+  color: white;
+  padding: 10px;
+  border-radius: 10px 10px 0 0;
+}
+.list-group-item {
+  padding: 10px;
+}
+</style>

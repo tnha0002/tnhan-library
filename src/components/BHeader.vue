c36290bd619180205eaae6df91de8c8de48a7d64
Tri Thong Nhan <tnha0002@student.monash.edu>
2024-09-30 08:36:40 +1000

    Completed Week 9 Applied (P/C)

--- src/components/BHeader.vue
+++ src/components/BHeader.vue
@@ -25,6 +25,11 @@
         <li class="nav-item">
           <router-link to="/addbook" class="nav-link" active-class="active"> Add Book </router-link>
         </li>
+        <li class="nav-item">
+          <router-link to="/GetBookCount" class="nav-link" active-class="active">
+            Get Book Count
+          </router-link>
+        </li>
       </ul>
     </header>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import AboutView from '@/views/AboutView.vue'
 import FirebaseSigninView from '@/views/FirebaseSigninView.vue'
 import FirebaseRegisterView from '@/views/FirebaseRegisterView.vue'
 import AddBookView from '@/views/AddBookView.vue'
+import GetBookCountView from '@/views/GetBookCountView.vue'
 
 const routes = [
   {
@@ -32,6 +33,11 @@ const routes = [
     path: '/addbook',
     name: 'AddBook',
     component: AddBookView
+  },
+  {
+    path: '/GetBookCount',
+    name: 'GetBookCount',
+    component: GetBookCountView
   }
 ]
 

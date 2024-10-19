030bad7a76c6e006a113b4f5f5117580d158afdd
Tri Thong Nhan <tnha0002@student.monash.edu>
2024-10-19 13:18:19 +1100

    9th attempt

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import ghPages from 'vite-plugin-gh-pages'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ghPages()],
+  base: '/tnhan-library/',
   build: {
     outDir: 'dist'
   },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: '/tnhan-library/'
+  }
 })
